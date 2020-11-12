1d86726ebb52089ef09cb41c21121a35eb19cc8c
Andre-Junior <andrejunior.dias@gmail.com>
2020-11-12 18:51:24 -0300

    adc btn

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-let time = 5000;
+let time = 9000;
 let imageIndex = 0;
 let imagens = document.querySelectorAll("#slider img");
 let max = imagens.length;
 
-const nextImagem = () => {
+let sliderNext = document.querySelector("#slider-next")
+let sliderPrev = document.querySelector("#slider-prev")
+
+
+
+const classImage = () => {
 
     imagens[imageIndex].classList.remove('active')
 
@@ -18,8 +23,27 @@ const nextImagem = () => {
 
 const start = () => {
     setInterval(() => {
-        nextImagem()
+        classImage()
     }, time)
 }
 
+
+
+
+const prev = () => {
+    
+    imagens[imageIndex].classList.remove('active')
+
+    imageIndex--
+
+    if(imageIndex < 0) {
+        imageIndex = 4
+    }
+
+    imagens[imageIndex].classList.add('active')
+}
+
+
+sliderNext.addEventListener("click", classImage)
+sliderPrev.addEventListener("click", prev)
 window.addEventListener('load', start)
